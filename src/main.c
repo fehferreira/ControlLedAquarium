71a2d26f1be7eb57ecbf5d713e57a8e27ea2fe56
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-04-26 16:53:28 -0300

    Developed function to update of values from eeprom
    A function will read a values from positions of eeprom memory and update values to compare and working whit then.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
 #define RESISTOR_PORT RA0_bit
 
+#define INITIAL_POSITION_EEPROM 0x0000
+#define QUANT_VALUE_EEPROM      0x0001
+#define READ_CONTROL_LED        0x0002
+#define LAST_POSITION_EEPROM    0x001A
+
+volatile unsigned short quant_values_eeprom,
+                        last_value_eeprom,
+                        read_control_led;
+
 unsigned readValueResistor(char portRead){
     return ADC_Read(portRead);
+}
+
+void updatingEepromData(){
+    quant_values_eeprom = EEPROM_Read(QUANT_VALUE_EEPROM);
+    read_control_led    = EEPROM_Read(READ_CONTROL_LED);
+    last_value_eeprom   = EEPROM_Read(LAST_POSITION_EEPROM);
 }
 
 void initializePic(){
@@ -11,12 +26,13 @@ void initializePic(){
     PORTB = 0x00000000;
 
     ADC_Init();
+    updatingEepromData();
 }
 
 void main(){
     initializePic();
         
     while(1){
-        PORTB = readValueResistor(RESISTOR_PORT);
+
     }
 }
