3659649fbc0c29d525919d614e2f8e23db60d53f
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-04-26 14:31:52 -0300

    Insert a serial library
    Configure a serial library to test recieve of ADC value

--- src/main.c
+++ src/main.c
@@ -1,20 +1,30 @@
-#define RESISTORPORT RB1_bit;
+#define RESISTOR_PORT RB1_bit;
+
+#define RX_PIN 7;
+#define TX_PIN 6;
+#define USART_BAUDRATE 9600;
+#define INVERT_FLAG 0;
+
+unsigned readValueResistor(unsigned short portReader){
+    return ADC_Read(portReader);
+}
 
 void initializePic(){
     TRISA = 0x11111111;
     TRISB = 0x11111110;
     PORTA = 0x00000000;
     PORTB = 0x00000000;
-}
 
-unsigned readValueResistor(unsigned short portReader){
-    return ADC_Read(portReader);
+    ADC_Init();
+    Soft_UART_Init(&PORTB,RX_PIN,TX_PIN,USART_BAUDRATE,INVERT_FLAG);
 }
 
 void main(){
+    unsigned value = 0;
     initializePic();
         
     while(1){
-        readValueResistor(RESISTORPORT);
+        value = readValueResistor(RESISTOR_PORT);
+        Soft_UART_Write(value);
     }
 }
